391e5ae9939c792c9b2083ed287bcaa51f672ae8
ALEJ2311 <jesusalej23@gmail.com>
2024-11-07 11:49:13 -0500

    Avance1

--- Proyecto-Taller5/src/Ejercicio1_EdadVotar.java
+++ Proyecto-Taller5/src/Ejercicio1_EdadVotar.java
@@ -1,5 +1,23 @@
+/***
+ * Edad para Votar: Determinar si una persona es elegible para 
+ * votar en función de su edad minimo 18 años = (mayor o igual a 18 años).
+ * @author Jesús Rivas
+ */
+import java.util.Scanner;
 public class Ejercicio1_EdadVotar {
     public static void main(String[] args) {
-        
+        int edad;
+        Scanner tcl = new Scanner(System.in);
+        System.out.print("DAME EDAD: ");
+        edad = tcl.nextInt();
+        if(edad >= 18)
+            System.out.println("SI PUEDE VOTAR");
+        else
+            System.out.println("NO PUEDE VOTAR");
     }
 }
+/***
+ * run:
+ * DAME EDAD: 50
+ * SI PUEDE VOTAR
+ */
