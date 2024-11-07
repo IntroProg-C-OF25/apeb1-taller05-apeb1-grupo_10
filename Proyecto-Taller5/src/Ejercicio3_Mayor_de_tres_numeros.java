e0089c3151cd7df0f19b461e227f39df789b2579
ALEJ2311 <jesusalej23@gmail.com>
2024-11-07 12:16:01 -0500

    Ejercicio3

--- Proyecto-Taller5/src/Ejercicio3_Mayor_de_tres_numeros.java
+++ Proyecto-Taller5/src/Ejercicio3_Mayor_de_tres_numeros.java
@@ -9,7 +9,7 @@ public class Ejercicio3_Mayor_de_tres_numeros {
     public static void main(String[] args) {
         int num1, num2, num3, n_mayor = 0;
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Ingrese un primer numero: ");
+        System.out.print("Ingrese un primer número: ");
         num1 = teclado.nextInt();
         System.out.print("Ingrese un segundo numero: ");
         num2 = teclado.nextInt();
@@ -18,14 +18,14 @@ public class Ejercicio3_Mayor_de_tres_numeros {
         if(num1 > num2){
             if(num1 > num3)
                 n_mayor = num1;
-            System.out.println("El numero mayor entre: " + num1 + "," + num2 + ","+ num3 + "es :"  + n_mayor);
+            System.out.println("El numero mayor entre: " + num1 + "," + num2 + ","+ num3 + " es :"  + n_mayor);
         }else{
             if(num2 > num3){
                 n_mayor = num2;
                 System.out.println("El numero mayor entre " + num1 + "," + num2 + ","+ num3 + " es: "  + n_mayor);
             }else{
                 n_mayor = num3;
-                System.out.println("El numero mayor entre: " + num1 + "," + num2 + ","+ num3 + "es :"  + n_mayor);
+                System.out.println("El numero mayor entre: " + num1 + "," + num2 + ","+ num3 + " es :"  + n_mayor);
             }
         }
     }
